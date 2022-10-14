336d7bd6511320958633f2113f66ea7c1aff4dfd
biometricPsychography <cyclinglinguist@gmail.com>
2022-10-14 08:49:28 -0500

    Work on base62 convertor for shortened url

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+const dns = require('node:dns');
+
+/*
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const PORT = 8080;
 
 app.use(cors());
 
@@ -14,11 +18,62 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let tempRedirectPathsArr = [];
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function (req, res) {
+
+    if (req.body.url.slice(0, 7) === 'http://' || req.body.url.slice(0, 8) === 'https://') {
+
+        dns.lookup(req.body.url, (err, address, family) => {
+            err ? console.log(err) : console.log(address);
+
+            res.json({ greeting: req.body.url });
+        })
+
+        
+
+    }
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}`);
+});*/
+
+
+// account for 6 spaces between lower and upper case
+function convertFromBase62(string) {
+
+    (typeof string === 'string') ? null : throwErr('argument should be string');
+
+    const base = 62;
+    let decimalValueOfCharCodeA = 65;
+    let uppercaseCharCodeOffset = -54;
+    let lowercaseCharCodeOffset = -60;
+    let runningSum = 0;
+    
+
+    for (let i = string.length -1; i > -1; i--) {
+        let possibleArabicNumeral = parseInt(string[i], 10);
+      /*  let base10Value = (string[i].charCodeAt(0)) ? string[i].charCodeAt(0) + charCodeOffset : */
+        if (possibleArabicNumeral) {
+            
+            runningSum += possibleArabicNumeral * Math.pow(62, string.length - i - 1);
+        } else if (base10Value > 10 && base10Value < 63) {
+
+            runningSum += base10Value * Math.pow(62, string.length - i - 1);
+        }
+
+        
+    }
+
+    return runningSum;
+}
+
+//console.log(convertFromBase62('A'));
+
+function throwErr(errMessage) {
+    throw Error(errMessage)
+}
+
+console.log('A'.charCodeAt(0))
